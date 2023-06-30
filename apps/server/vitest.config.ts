7833123b129e926f58a1a9257fe68389f9366b7c
kluzko <kluzniakkuba@gmail.com>
2023-06-30 09:26:25 +0200

    chore: simplfy implmentation of relative paths in vitest config

--- apps/server/vitest.config.ts
+++ apps/server/vitest.config.ts
@@ -1,3 +1,4 @@
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
@@ -5,14 +6,5 @@ export default defineConfig({
       setupFiles: ['setup.test.ts'],
       testTimeout: 5000, // 5 seconds
    },
-   resolve: {
-      alias: {
-         prisma: 'src/prisma/index',
-         common: 'src/common',
-         modules: 'src/modules',
-         container: 'src/container',
-         server: 'src/server',
-         utils: 'src/utils',
-      },
-   },
+   plugins: [tsconfigPaths()],
 })
