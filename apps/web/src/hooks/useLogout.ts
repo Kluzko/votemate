1eba154942cf657496a81649b8d4d85a883f7ee1
kluzko <kluzniakkuba@gmail.com>
2023-07-04 22:20:21 +0200

    refactor: add optimistic update in logout

--- apps/web/src/hooks/useLogout.ts
+++ apps/web/src/hooks/useLogout.ts
@@ -1,16 +1,34 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
 
 const logoutUser = async () => axios.post('/api/logout')
 
 export const useLogout = () => {
    const client = useQueryClient()
    const { mutate: logout } = useMutation(logoutUser, {
+      onMutate: async () => {
+         // Snapshot the previous value
+         const previousAuthStatus = client.getQueryData(['authStatus'])
+
+         // Optimistically update to the new value
+         client.setQueryData(['authStatus'], false)
+
+         // Return the snapshotted value
+         return { previousAuthStatus }
+      },
+      onError: (error, _variables, context) => {
+         toast.error('Someting went wrong,try again')
+         // If the mutation fails, rollback to the previous value
+         if (context?.previousAuthStatus) {
+            client.setQueryData(['authStatus'], context.previousAuthStatus)
+         }
+      },
       onSuccess: () => {
+         // Invalidate the query to refetch it
          client.invalidateQueries(['authStatus'])
          return window.navigate({ to: '/' })
       },
-      onError: error => console.log(error),
    })
 
    return { logout }
