7ad8669b167428e6305a18dcfc438a9f0571b946
kluzko <kluzniakkuba@gmail.com>
2023-07-05 12:58:13 +0200

    refactor: make getRemaining time more readable

--- apps/web/src/utils/getRemainingTime.ts
+++ apps/web/src/utils/getRemainingTime.ts
@@ -1,31 +1,19 @@
-export const getRemainingTime = (endDate: Date): string => {
-   if (!(endDate instanceof Date) || isNaN(endDate.getTime())) {
-      throw new Error('Invalid date')
-   }
+export const getRemainingTime = (endDateStr: string) => {
+   const timeDifference = new Date(endDateStr).getTime() - Date.now()
 
-   const timeDifference = endDate.getTime() - Date.now()
+   if (timeDifference < 0) return 'Expired'
 
-   if (timeDifference < 0) {
-      return 'Expired'
-   }
+   const [minutes, hours, days, months] = [
+      (timeDifference / (60 * 1000)) % 60,
+      (timeDifference / (60 * 60 * 1000)) % 24,
+      (timeDifference / (24 * 60 * 60 * 1000)) % 30,
+      timeDifference / (30 * 24 * 60 * 60 * 1000),
+   ].map(Math.floor)
 
-   const [millisecondsPerMinute, millisecondsPerHour, millisecondsPerDay] = [
-      60 * 1000,
-      60 * 60 * 1000,
-      24 * 60 * 60 * 1000,
-   ]
+   if (months > 0) return `${months} month${months > 1 ? 's' : ''}`
+   if (days > 0) return `${days} day${days > 1 ? 's' : ''}`
+   if (hours > 0) return `${hours} hour${hours > 1 ? 's' : ''}`
+   if (minutes > 0) return `${minutes} minute${minutes > 1 ? 's' : ''}`
 
-   const [days, hours, minutes] = [
-      Math.floor(timeDifference / millisecondsPerDay),
-      Math.floor((timeDifference % millisecondsPerDay) / millisecondsPerHour),
-      Math.floor((timeDifference % millisecondsPerHour) / millisecondsPerMinute),
-   ]
-
-   return days > 0
-      ? `${days} day${days > 1 ? 's' : ''}`
-      : hours > 0
-      ? `${hours} hour${hours > 1 ? 's' : ''}`
-      : minutes > 0
-      ? `${minutes} minute${minutes > 1 ? 's' : ''}`
-      : 'Less than a minute'
+   return 'Less than a minute'
 }
