830b4de9270b711736b5deefa394936b26237509
kluzko <kluzniakkuba@gmail.com>
2023-07-11 16:41:33 +0200

    reafactor: make it more readable

--- apps/web/src/utils/getRemainingTime.ts
+++ apps/web/src/utils/getRemainingTime.ts
@@ -1,19 +1,22 @@
+const pluralize = (value: number, unit: string): string => `${value} ${unit}${value > 1 ? 's' : ''}`
+
 export const getRemainingTime = (endDateStr: string) => {
    const timeDifference = new Date(endDateStr).getTime() - Date.now()
 
    if (timeDifference < 0) return 'Expired'
 
-   const [minutes, hours, days, months] = [
+   const [seconds, minutes, hours, days, months] = [
+      (timeDifference / 1000) % 60,
       (timeDifference / (60 * 1000)) % 60,
       (timeDifference / (60 * 60 * 1000)) % 24,
       (timeDifference / (24 * 60 * 60 * 1000)) % 30,
       timeDifference / (30 * 24 * 60 * 60 * 1000),
    ].map(Math.floor)
 
-   if (months > 0) return `${months} month${months > 1 ? 's' : ''}`
-   if (days > 0) return `${days} day${days > 1 ? 's' : ''}`
-   if (hours > 0) return `${hours} hour${hours > 1 ? 's' : ''}`
-   if (minutes > 0) return `${minutes} minute${minutes > 1 ? 's' : ''}`
+   if (months > 0) return pluralize(months, 'month')
+   if (days > 0) return pluralize(days, 'day')
+   if (hours > 0) return `${pluralize(hours, 'hour')} ${minutes > 14 ? '' : ` ${pluralize(minutes, 'minute')}`}`
+   if (minutes > 0) return `${pluralize(minutes, 'minute')} ${minutes > 15 ? '' : ` ${pluralize(seconds, 'second')}`}`
 
-   return 'Less than a minute'
+   return pluralize(seconds, 'second')
 }
