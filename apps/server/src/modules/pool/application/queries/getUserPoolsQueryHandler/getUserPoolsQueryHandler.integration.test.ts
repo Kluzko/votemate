8b5843d325b6b9c74b6d935b4485b6096e1c6364
kluzko <kluzniakkuba@gmail.com>
2023-07-13 17:43:02 +0200

    test: add getUserPools test

---
+++ apps/server/src/modules/pool/application/queries/getUserPoolsQueryHandler/getUserPoolsQueryHandler.integration.test.ts
@@ -0,0 +1,78 @@
+import { ContainerSingleton } from 'container'
+import { mockPoolData } from 'modules/pool/infrastructure/test-utils'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { NotFoundError } from 'common/errors'
+
+import { type CreatePool, type PoolData } from 'modules/pool/api/schemas'
+
+import { type PoolRepository } from 'modules/pool/infrastructure/repositories'
+
+import { symbols } from 'modules/pool/symbols'
+
+import { type GetUserPoolsQueryHandler } from './getUserPoolsQueryHandler'
+
+describe('GetUserPoolsQueryHandler', () => {
+   let getUserPoolsQueryHandler: GetUserPoolsQueryHandler
+   let poolRepository: PoolRepository
+   let userPool: CreatePool
+   let createdPoolId: string
+
+   beforeAll(() => {
+      const container = ContainerSingleton.getInstance()
+      getUserPoolsQueryHandler = container.get(symbols.getUserPoolsQueryHandler)
+      poolRepository = container.get(symbols.poolRepository)
+   })
+
+   describe('.execute', () => {
+      let result: { pools: (PoolData & { totalVotes: number })[] }
+
+      beforeAll(async () => {
+         userPool = mockPoolData.createBasePoolData()
+
+         const { pool } = await poolRepository.createPool(userPool)
+         createdPoolId = pool.getId()
+
+         result = await getUserPoolsQueryHandler.execute({ userId: userPool.userId })
+      })
+
+      afterAll(async () => {
+         await poolRepository.deletePool({
+            id: createdPoolId,
+            userId: userPool.userId,
+         })
+      })
+
+      it('should return user pools', () => {
+         // TODO: make omit method for mock data
+         // eslint-disable-next-line @typescript-eslint/no-unused-vars
+         const { userId, ...payloadNoUserId } = userPool
+
+         expect(result.pools).toEqual(
+            expect.arrayContaining([
+               {
+                  ...payloadNoUserId,
+                  id: expect.any(String),
+                  answers: expect.arrayContaining(
+                     payloadNoUserId.answers.map(value => ({
+                        id: expect.any(String),
+                        value,
+                     }))
+                  ),
+                  totalVotes: 0,
+               },
+            ])
+         )
+      })
+
+      it('should throw NotFoundError when there are no user pools', async () => {
+         try {
+            await getUserPoolsQueryHandler.execute({ userId: 'bad-user-id' })
+         } catch (error) {
+            return expect(error).toBeInstanceOf(NotFoundError)
+         }
+
+         expect.fail()
+      })
+   })
+})
